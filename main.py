89d16cb0f90ade7e8d1af72411b31221b5a2c836
Bucketchi <alexandretcoutinho@gmail.com>
2023-06-09 17:38:28 +0100

    Added command line argument functionality

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from movie_app import MovieApp
 from storage_json import StorageJson
 from storage_csv import StorageCsv
+import sys
 
-storage = StorageJson('data.json')
+if sys.argv[1].endswith(".json"):
+    storage = StorageJson(sys.argv[1])
+elif sys.argv[1].endswith(".csv"):
+    storage = StorageCsv(sys.argv[1])
+
 movie_app = MovieApp(storage)
 movie_app.run()
